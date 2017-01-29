7af8d259bc78a868136d86827bd1e657a0095583
Jean-Philippe Belanger <jean-philippe.belanger@ericsson.com>
2017-01-29 09:46:47 -0500

    Override...
    Change-Id: Ia18333491d4093783f6f093e458df2c2c6976ffc

---
+++ Overrider/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Overrider
+{
+    class Personne
+    {
+        private String name;
+        private int age;
+        private Personne conjoint;
+
+        public Personne(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+
+        public void Marier(Personne p)
+        {
+            if (p == this || p == null || p.conjoint != null )
+            {
+                throw new ArgumentException("Beurk...");
+            }
+
+            conjoint = p;
+            p.conjoint = this;
+        }
+
+        public static void Main(string[] args)
+        {
+            Personne p1 = new Personne("Paul", 18);
+            Personne p2 = new Personne("Paul", 18);
+
+            p2.Marier(p1);
+            Console.WriteLine(p1);
+        }
+    }
+}
