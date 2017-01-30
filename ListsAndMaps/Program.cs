040accb373bb68d791de09467181add8f198b0a5
Jean-Philippe Belanger <jean-philippe.belanger@ericsson.com>
2017-01-30 16:09:01 -0500

    Lists and Maps
    Change-Id: I3a4c6a79e39d2e954788f70ab32d6759591bb2c0

---
+++ ListsAndMaps/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Xsl.Runtime;
+
+namespace ListsAndMaps
+{
+    class Fichier
+    {
+        private string nom;
+        private List<string> contenu = new List<string>();
+
+        public Fichier(string nom)
+        {
+            this.nom = nom;
+        }
+
+
+        public void AjouterContenu(string contenu)
+        {
+            this.contenu.Add(contenu);
+        }
+
+        public void Cat()
+        {
+            foreach (var ligne in contenu)
+            {
+                Console.WriteLine(nom + ": " + ligne);
+            }
+        }
+    }
+
+    class Repertoire
+    {
+        private Dictionary<string, Fichier> fichiers = new Dictionary<string, Fichier>();
+
+        public Repertoire()
+        {
+        }
+
+        public Fichier NouveauFichier(string nom)
+        {
+            Fichier f = new Fichier(nom);
+            fichiers.Add(nom, f);
+
+            return f;
+        }
+
+        public static void Main(string[] args)
+        {
+
+            Repertoire r = new Repertoire();
+            Fichier f = r.NouveauFichier("foo");
+            f.AjouterContenu("Moomoo");
+            f.AjouterContenu("googoo");
+
+            f.Cat();
+        }
+    }
+}
