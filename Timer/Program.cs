7aca6e5cbede43b4271f1f4185f65b3ad355e651
Jean-Philippe Belanger <jean-philippe.belanger@ericsson.com>
2017-02-27 14:00:16 -0500

    Timer!
    Change-Id: I0b7bdf77c94c4edb5472db98ff418bc8ad79b40a

---
+++ Timer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace Timer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(10000);
+            timer.Elapsed += new ElapsedEventHandler((source, eventObject) =>{System.Environment.Exit(200);});
+            timer.Start();
+
+            Console.WriteLine("Hello ?");
+            Console.ReadKey();
+        }
+    }
+}
